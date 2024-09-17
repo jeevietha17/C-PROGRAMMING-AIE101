44674c7ef28ad7d21fde4fa62ecae8e06f4cabb9
jeevietha17 <BL.SC.U4AIE24017@BL.STUDENTS.AMRITA.EDU>
2024-09-17 10:30:08 +0530

    Add files via upload

---
+++ temp.c
@@ -0,0 +1,15 @@
+int main() 
+{
+    float fahrenheit;
+    float celsius;
+
+    
+    printf("Enter the temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    printf("The temperature in Celsius is: %.2f\n", celsius);
+
+    return 0;
+}
